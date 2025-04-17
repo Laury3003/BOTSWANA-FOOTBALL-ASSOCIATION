138bacfb8e11fe90cefdc16eed5f95c30772c6ad
Amo <201804196@ub.ac.bw>
2025-04-17 14:27:14 +0200

    Created base class Sport with neccessary field, constructor ,getters & setters.

---
+++ Sport.java
@@ -0,0 +1,19 @@
+public class Sport
+{
+    private String sportName;
+
+    public Sport(String sportName)
+    {
+        this.sportName = sportName;
+    }
+
+    public String getSportName()
+    {
+        return sportName;
+    }
+
+    public void setSportName(String sportName)
+    {
+        this.sportName = sportName;
+    }
+}

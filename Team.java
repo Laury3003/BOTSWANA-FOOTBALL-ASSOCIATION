cffa46281aff8901e5b34839663c89fa5c49962f
Miko-19 <godfreymoyo2005@gmail.com>
2025-04-24 18:50:23 +0200

    Update Team.java
    I added and encapsulated an Array List for players .
    then added getters for it

--- Team.java
+++ Team.java
@@ -1,20 +1,38 @@
+import java.util.ArrayList;
+
 public class Team extends Sport{
     private String teamName;
     private String teamID;
     private String establishment;
     private String origin;
-    private String teamPlayers;
     private int rank;
+    private ArrayList<Player> players = new ArrayList<>();
 
-    public Team(String sportName, String teamName, String teamID, String establishment, String origin, String teamPlayers, int rank){
+    public Team(String sportName, String teamName, String teamID, String establishment, String origin, int rank){
         super(sportName);
         this.teamName = teamName;
         this.teamID = teamID;
         this.establishment = establishment;
-        this.origin = origin;
-        this.teamPlayers = teamPlayers;
-        this.rank = rank; 
+        this.origin = origin; 
     }
+
+    public void addPlayer(Player player) {
+        players.add(player);
+        calculateRank();
+    }
+    
+    public void calculateRank() {
+        if (players == null || players.isEmpty()) {
+            this.rank = 0;
+            return;
+        }
+        double total = 0;
+        for (Player p : players) {
+            total += p.getRating();
+        }
+        this.rank = (int)(total / players.size());
+    }
+    
     public String getTeamName(){
         return teamName;
     }
@@ -39,30 +57,14 @@ public class Team extends Sport{
     public void setOrigin(String origin){
         this.origin = origin;
     }
-    public String getTeamPlayers(){
-        return teamPlayers;
-    }
-    public void setTeamPlayers(String teamPlayers){
-    this.teamPlayers = teamPlayers;
-}
     public int getRank(){
         return rank;
     }
     public void setRank(int rank){
         this.rank = rank;
     }
-    public void calculateRank() {
-        if (teamPlayers == 0 || teamPlayers.isEmpty()) {
-            this.rank = 0;
-            return;
-        }
-
-        double total = 0;
-        for (Player p : teamPlayers) {
-            total += p.getRating();
-        }
-
-        this.rank = (int)(total / teamPlayers.size());
+    public ArrayList <Player> getPlayers() {
+        return players;
     }
 
 }

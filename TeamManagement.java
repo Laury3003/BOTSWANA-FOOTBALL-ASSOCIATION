f36c24bbd13d364f1599f3d908ff88f01947bd28
Miko-19 <godfreymoyo2005@gmail.com>
2025-04-30 12:58:13 +0200

    Update TeamManagement.java

--- TeamManagement.java
+++ TeamManagement.java
@@ -1,8 +1,3 @@
-// TeamManager.java
-// This interface defines the blueprint for managing teams in the football system.
-// It ensures any implementing class provides core functionality for team input,
-// displaying team rankings, and showing key player insights.
-
 public interface TeamManagement {
     void inputTeamData();
     void displayRankings();

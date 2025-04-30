70ce81eb09e1a1799b899d3c9aab211a26c47367
Omphemetse Mpatane <mpataneomphemetse@gmail.com>
2025-04-30 13:33:59 +0200

    Created an abstract class called TeamDataHandler

---
+++ TeamDataHandler.java
@@ -0,0 +1,7 @@
+// TeamDataHandler.java
+// Abstract base class that holds a shared reference to an array of teams.
+// Implements the TeamManager interface which defines operations related to team management.
+
+public abstract class TeamDataHandler implements TeamManager {
+    protected Team[] teams;
+}

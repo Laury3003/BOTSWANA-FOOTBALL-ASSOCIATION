21313f5211f42d833e76848f53bf00d188833683
Miko-19 <godfreymoyo2005@gmail.com>
2025-04-30 19:49:43 +0200

    Update Team.java

--- Team.java
+++ Team.java
@@ -1,70 +1,67 @@
 import java.util.ArrayList;
 
-public class Team extends Sport{
-    private String teamName;
-    private String teamID;
-    private String establishment;
-    private String origin;
-    private int rank;
-    private ArrayList<Player> players = new ArrayList<>();
+public class Team {
+   private ArrayList<Player> players;
+  
+   public Team(String name) {
+      this.name = name;
+      this.players = new ArrayList<Player>();
+   }
 
-    public Team(String sportName, String teamName, String teamID, String establishment, String origin, int rank){
-        super(sportName);
-        this.teamName = teamName;
-        this.teamID = teamID;
-        this.establishment = establishment;
-        this.origin = origin; 
-    }
+   public void addPlayer(Player player) {
+      players.add(player);
+   }
 
-    public void addPlayer(Player player) {
-        players.add(player);
-        calculateRank();
-    }
-    
-    public void calculateRank() {
-        if (players == null || players.isEmpty()) {
-            this.rank = 0;
-            return;
-        }
-        double total = 0;
-        for (Player p : players) {
-            total += p.getRating();
-        }
-        this.rank = (int)(total / players.size());
-    }
-    
-    public String getTeamName(){
-        return teamName;
-    }
-    public void setTeamName(String teamName){
-        this.teamName = teamName;
-    }
-    public String getTeamID(){
-        return teamID;
-    }
-    public void setTeamID(String teamID){
-        this.teamID = teamID;
-    }
-    public String getEstablishment(){
-        return establishment;
-    }
-    public void setEstablishment(String establishment){
-        this.establishment = establishment;
-    }
-    public String getOrigin(){
-        return origin;
-    }
-    public void setOrigin(String origin){
-        this.origin = origin;
-    }
-    public int getRank(){
-        return rank;
-    }
-    public void setRank(int rank){
-        this.rank = rank;
-    }
-    public ArrayList <Player> getPlayers() {
-        return players;
-    }
+   public double getAverageRating() { // caculates the average of all player rating
+      double total = 0;
+      for (Player p : players) {
+         total += p.getRating();
+      }
+         if (players.size() > 0) {
+         return total / players.size();
+         
+      } else {
+         return 0;
+      }
+   }
+   public Player getBestPlayer(){ /// gets the player with highe rating
+      
+      if (players.isEmpty()) 
+         return null;
 
+      Player best = players.get(0); ///first player
+      for (Player p : players) {
+
+      if (p.getRating() > best.getRating()) {
+            best = p;
+         }
+      }
+      return best;
+   }
+
+   public Player getFavouritePlayer() {
+      for (Player p : players) {
+         if (p.isFavourite()) {
+            return p;
+         }
+      }
+      //return null if no favorite player is found
+      return null;
+   }
+
+   public void displayTeamInfo() {
+      System.out.println("Team: " + name);
+      //displays the information for each player in the team
+      for (Player p : players) {
+         System.out.println("- " + p.toString());
+      }
+   }
+
+   public String getName() {
+      return name;
+   }
+
+   public ArrayList<Player> getPlayers() {
+      return players;
+   }
 }

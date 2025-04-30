25776f2f42eb962e0c9a3fd5c6743586ce3d05d5
Miko-19 <godfreymoyo2005@gmail.com>
2025-04-30 20:13:32 +0200

    Update SearchUtility.java

--- SearchUtility.java
+++ SearchUtility.java
@@ -1,26 +1,11 @@
-/**
- * Utility class providing search methods for football data.
- */
-
-public class SearchUtility 
-{
-   /**
-    * Performs a linear search on an array of Player objects to find a player by name.
-    * The search is case-insensitive, so "John" will match "john" or "JOHN".
-    */
+public class SearchUtility {
    
-   public static int linearSearchByName(Player[] players, String name)
-   {
-      // Loop through the entire player array
+   public static int linearSearchByName(Player[] players, String name){
       for (int i = 0; i < players.length; i++) {
-         // Perform case-insensitive comparison of player names
          if (players[i].getName().equalsIgnoreCase(name)) {
-            // Return index position when match is found
             return i;
          }
       }
-      // Return -1 to indicate that no matching player was found
-      return -1;
+      return -1; //if i is not found
    }
-
 }

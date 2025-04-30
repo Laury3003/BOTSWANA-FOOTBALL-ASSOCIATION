3b5b8a46cf0628611a5c8f42db37d5afa4078724
Miko-19 <godfreymoyo2005@gmail.com>
2025-04-30 22:02:44 +0200

    Update TeamDataHandler.java
    final touches

--- TeamDataHandler.java
+++ TeamDataHandler.java
@@ -1,15 +1,7 @@
 import java.util.ArrayList;
 
-/**
- * Abstract class that implements the TeamManager interface.
- * This class provides a base implementation structure for team management operations.
- * It contains a protected ArrayList of teams that can be manipulated by subclasses.
- */
 public abstract class TeamDataHandler implements TeamManager {
-    /**
-     * Protected ArrayList to store the team objects.
-     * This data structure is accessible to all subclasses.
-     */
+    
     protected ArrayList<Team> teams;
     
 }

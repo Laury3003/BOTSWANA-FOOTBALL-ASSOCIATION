55c5f94db1816959fd8312360e3d17a62e8e2f2f
Omphemetse Mpatane <mpataneomphemetse@gmail.com>
2025-04-30 17:17:18 +0200

    Added more comments

--- TeamDataHandler.java
+++ TeamDataHandler.java
@@ -1,7 +1,15 @@
-// TeamDataHandler.java
-// Abstract base class that holds a shared reference to an array of teams.
-// Implements the TeamManager interface which defines operations related to team management.
+import java.util.ArrayList;
 
+/**
+ * Abstract class that implements the TeamManager interface.
+ * This class provides a base implementation structure for team management operations.
+ * It contains a protected ArrayList of teams that can be manipulated by subclasses.
+ */
 public abstract class TeamDataHandler implements TeamManager {
-    protected Team[] teams;
+    /**
+     * Protected ArrayList to store the team objects.
+     * This data structure is accessible to all subclasses.
+     */
+    protected ArrayList<Team> teams;
+    
 }
